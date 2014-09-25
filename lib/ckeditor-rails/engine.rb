01b23b98416291c4ac3f6ccff58a0fcab9c90243
joseen <qs.joseen@gmail.com>
2014-09-25 13:48:33 +0800

    remove default precompile

--- lib/ckeditor-rails/engine.rb
+++ lib/ckeditor-rails/engine.rb
@@ -1,16 +1,16 @@
 module Ckeditor
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'ckeditor.assets.precompile', group: :all do |app|
-        app.config.assets.precompile += %W(
-          ckeditor/*.js
-          ckeditor/*.css
-          ckeditor/*.png
-          ckeditor/*.gif
-          ckeditor/*.html
-          ckeditor/*.md
-        )
-      end
+      # initializer 'ckeditor.assets.precompile', group: :all do |app|
+      #   app.config.assets.precompile += %W(
+      #     ckeditor/*.js
+      #     ckeditor/*.css
+      #     ckeditor/*.png
+      #     ckeditor/*.gif
+      #     ckeditor/*.html
+      #     ckeditor/*.md
+      #   )
+      # end
 
       rake_tasks do
         load "ckeditor-rails/tasks.rake"
